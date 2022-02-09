92319d5ac12ebc62cb3aafa0fcdaa3bfd56e88d1
anquii <50277099+anquii@users.noreply.github.com>
2022-02-09 17:26:08 +0000

    Updated ChildKeyIndexValidatorTests

--- Tests/BIP32Tests/ChildKeyIndexValidatorTests.swift
+++ Tests/BIP32Tests/ChildKeyIndexValidatorTests.swift
@@ -7,59 +7,109 @@ final class ChildKeyIndexValidatorTests: XCTestCase {
     }
 
     func testGivenNormalIndex_AndUnhardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(0x1, isHardened: false)
+        testValidIndex(
+            0x1,
+            isHardened: false
+        )
     }
 
     func testGivenNormalLowerBoundIndex_AndUnhardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(ChildKeyDerivationRange.normal.lowerBound, isHardened: false)
+        testValidIndex(
+            ChildKeyDerivationRange.normal.lowerBound,
+            isHardened: false
+        )
     }
 
     func testGivenNormalUpperBoundIndex_AndUnhardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(ChildKeyDerivationRange.normal.upperBound, isHardened: false)
+        testValidIndex(
+            ChildKeyDerivationRange.normal.upperBound,
+            isHardened: false
+        )
     }
 
     func testGivenHardenedIndex_AndHardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(0x80000001, isHardened: true)
+        testValidIndex(
+            0x80000001,
+            isHardened: true
+        )
     }
 
     func testGivenHardenedLowerBoundIndex_AndHardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(ChildKeyDerivationRange.hardened.lowerBound, isHardened: true)
+        testValidIndex(
+            ChildKeyDerivationRange.hardened.lowerBound,
+            isHardened: true
+        )
     }
 
     func testGivenHardenedUpperBoundIndex_AndHardenedIndication_WhenValidate_ThenValidIndex() {
-        testValidIndex(ChildKeyDerivationRange.hardened.upperBound, isHardened: true)
+        testValidIndex(
+            ChildKeyDerivationRange.hardened.upperBound,
+            isHardened: true
+        )
     }
 
     func testGivenNormalIndex_AndHardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(0x1, isHardened: true)
+        testInvalidIndex(
+            0x1,
+            isHardened: true,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenNormalLowerBoundIndex_AndHardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.normal.lowerBound, isHardened: true)
+        testInvalidIndex(
+            ChildKeyDerivationRange.normal.lowerBound,
+            isHardened: true,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenNormalUpperBoundIndex_AndHardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.normal.upperBound, isHardened: true)
+        testInvalidIndex(
+            ChildKeyDerivationRange.normal.upperBound,
+            isHardened: true,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenNormalUpperBoundIndexPlus1_AndUnhardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.normal.upperBound + 1, isHardened: false)
+        testInvalidIndex(
+            ChildKeyDerivationRange.normal.upperBound + 1,
+            isHardened: false,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenHardenedIndex_AndUnhardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(0x80000001, isHardened: false)
+        testInvalidIndex(
+            0x80000001,
+            isHardened: false,
+            expectedError: .invalidIndex
+        )
    }
 
     func testGivenHardenedLowerBoundIndex_AndUnhardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.hardened.lowerBound, isHardened: false)
+        testInvalidIndex(
+            ChildKeyDerivationRange.hardened.lowerBound,
+            isHardened: false,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenHardenedLowerBoundIndexMinus1_AndHardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.hardened.lowerBound - 1, isHardened: true)
+        testInvalidIndex(
+            ChildKeyDerivationRange.hardened.lowerBound - 1,
+            isHardened: true,
+            expectedError: .invalidIndex
+        )
     }
 
     func testGivenHardenedUpperBoundIndex_AndUnhardenedIndication_WhenValidate_ThenThrowInvalidIndexError() {
-        testInvalidIndex(ChildKeyDerivationRange.hardened.upperBound, isHardened: false)
+        testInvalidIndex(
+            ChildKeyDerivationRange.hardened.upperBound,
+            isHardened: false,
+            expectedError: .invalidIndex
+        )
     }
 }
 
@@ -71,13 +121,13 @@ extension ChildKeyIndexValidatorTests {
         )
     }
 
-    func testInvalidIndex(_ index: UInt32, isHardened: Bool) {
+    func testInvalidIndex(_ index: UInt32, isHardened: Bool, expectedError: ChildKeyIndexError) {
         XCTAssertThrowsError(
             try sut().validateIndex(index, isHardened: isHardened)
         ) { error in
             XCTAssertEqual(
                 error as! ChildKeyIndexError,
-                ChildKeyIndexError.invalidIndex
+                expectedError
             )
         }
     }
