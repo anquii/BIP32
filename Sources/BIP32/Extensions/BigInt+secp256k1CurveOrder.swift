d528d708459aac6eb953f6ca74e0b79d0917439a
anquii <50277099+anquii@users.noreply.github.com>
2022-02-28 16:25:04 +0000

    Added BigInt+secp256k1CurveOrder

---
+++ Sources/BIP32/Extensions/BigInt+secp256k1CurveOrder.swift
@@ -0,0 +1,8 @@
+import BigInt
+
+extension BigUInt {
+    static let secp256k1CurveOrder = BigUInt(
+        "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
+        radix: 16
+    )!
+}
