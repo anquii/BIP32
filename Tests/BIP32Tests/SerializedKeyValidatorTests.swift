58815985f5c225ea3702f65ddf86e80cd105d21b
anquii <50277099+anquii@users.noreply.github.com>
2022-03-23 19:16:22 +0000

    Updated SerializedKeyValidatorTests

--- Tests/BIP32Tests/SerializedKeyValidatorTests.swift
+++ Tests/BIP32Tests/SerializedKeyValidatorTests.swift
@@ -1,30 +1,56 @@
+import Foundation
 import XCTest
 import BIP32
 
 final class SerializedKeyValidatorTests: XCTestCase {
-    private var serializedKeyCoder: SerializedKeyCoder!
-    private var testVectors: [SerializedKeyValidatorTestVector]!
+    private let serializedKeyCoder = SerializedKeyCoder()
+    private let jsonDecoder = JSONDecoder()
+    private var invalidTestVectors: [SerializedKeyValidatorTestVector]!
+    private var validPrivateMasterKeyTestVectors: [MasterKeyTestVector]!
+    private var validPublicMasterKeyTestVectors: [MasterKeyTestVector]!
 
     override func setUpWithError() throws {
-        serializedKeyCoder = .init()
-        testVectors = try JSONDecoder().decode([SerializedKeyValidatorTestVector].self, from: serializedKeyValidatorTestData)
+        invalidTestVectors = try jsonDecoder.decode([SerializedKeyValidatorTestVector].self, from: serializedKeyValidatorTestData)
+        validPrivateMasterKeyTestVectors = try jsonDecoder.decode([MasterKeyTestVector].self, from: privateMasterKeyTestData)
+        validPublicMasterKeyTestVectors = try jsonDecoder.decode([MasterKeyTestVector].self, from: publicMasterKeyTestData)
     }
 
     private func sut() -> SerializedKeyValidator {
         .init()
     }
 
-    func testGivenVectorEncodedKey_WhenValidate_ThenThrowError() {
+    func testGivenVector_WithInvalidEncodedKey_WhenValidate_ThenThrowError() {
         let sut = self.sut()
 
-        for testVector in testVectors {
+        for testVector in invalidTestVectors {
             let keyAccessControl: KeyAccessControl = testVector.isPrivate ? .`private` : .`public`
             if let serializedKey = try? serializedKeyCoder.decode(string: testVector.base58CheckEncodedKey) {
                 XCTAssertThrowsError(
-                    try sut.validate(serializedKey: serializedKey, keyAccessControl: keyAccessControl),
-                    testVector.base58CheckEncodedKey
+                    try sut.validate(serializedKey: serializedKey, keyAccessControl: keyAccessControl)
                 )
             }
+        }
+    }
+
+    func testGivenValidEncodedPrivateKey_WhenValidate_ThenNoErrorThrown() throws {
+        let sut = self.sut()
+
+        for testVector in validPrivateMasterKeyTestVectors {
+            let serializedKey = try serializedKeyCoder.decode(string: testVector.base58CheckEncodedKey)
+            XCTAssertNoThrow(
+                try sut.validate(serializedKey: serializedKey, keyAccessControl: .`private`)
+            )
+        }
+    }
+
+    func testGivenValidEncodedPublicKey_WhenValidate_ThenNoErrorThrown() throws {
+        let sut = self.sut()
+
+        for testVector in validPublicMasterKeyTestVectors {
+            let serializedKey = try serializedKeyCoder.decode(string: testVector.base58CheckEncodedKey)
+            XCTAssertNoThrow(
+                try sut.validate(serializedKey: serializedKey, keyAccessControl: .`public`)
+            )
         }
     }
 }
