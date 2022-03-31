e2d6086529bfd8417e381febcbe15164d3db6a23
anquii <50277099+anquii@users.noreply.github.com>
2022-03-31 13:50:42 +0100

    Add AddressTestVector

---
+++ Tests/BIP32Tests/Helpers/AddressTestVector.swift
@@ -0,0 +1,7 @@
+struct AddressTestVector {
+    static let hexEncodedPublicKey = "0202a406624211f2abbdc68da3df929f938c3399dd79fac1b51b0e4ad1d26a47aa"
+    static let address = "1PRTTaJesdNovgne6Ehcdu1fpEdX7913CK"
+    static let version = UInt8(0)
+
+    private init() {}
+}
