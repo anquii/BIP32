a69e64d7b3c89a476066f0350c491033d0e44507
anquii <50277099+anquii@users.noreply.github.com>
2022-04-08 09:51:47 +0100

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -16,38 +16,36 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/anquii/CryptoSwiftWrapper.git",
-            from: "1.4.3"
+            url: "https://github.com/anquii/Base58Check.git",
+            .upToNextMajor(from: "1.0.0")
         ),
         .package(
             url: "https://github.com/attaswift/BigInt.git",
             .upToNextMajor(from: "5.3.0")
         ),
         .package(
-            name: "secp256k1",
-            url: "https://github.com/GigaBitcoin/secp256k1.swift.git",
-            .exact("0.5.0")
+            url: "https://github.com/anquii/CryptoSwiftWrapper.git",
+            from: "1.4.3"
         ),
         .package(
-            name: "RIPEMD160",
             url: "https://github.com/anquii/RIPEMD160.git",
             .upToNextMajor(from: "1.0.0")
         ),
         .package(
-            name: "Base58Check",
-            url: "https://github.com/anquii/Base58Check.git",
-            .upToNextMajor(from: "1.0.0")
+            name: "secp256k1",
+            url: "https://github.com/GigaBitcoin/secp256k1.swift.git",
+            .exact("0.5.0")
         )
     ],
     targets: [
         .target(
             name: "BIP32",
             dependencies: [
-                "CryptoSwiftWrapper",
+                "Base58Check",
                 "BigInt",
-                "secp256k1",
+                "CryptoSwiftWrapper",
                 "RIPEMD160",
-                "Base58Check"
+                "secp256k1"
             ]
         ),
         .testTarget(
